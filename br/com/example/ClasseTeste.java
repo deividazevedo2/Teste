8e94659c6acda8df110d845fc60fbf9a89634931
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-09 19:43:47 -0300

    new commit

--- br/com/example/ClasseTeste.java
+++ br/com/example/ClasseTeste.java
@@ -5,6 +5,7 @@ public class ClasseTeste {
 	public static void main(String[] args) {
 		System.out.println("Testando um push na tag");
 		System.out.println("Test master");
+		System.out.println("Commit");
 
 	}
 
