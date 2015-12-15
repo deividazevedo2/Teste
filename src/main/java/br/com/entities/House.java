ac84884a2574dc70133366bebd2dab3de8dbaf33
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-15 13:02:07 -0300

    Adicionando a entidade House

---
+++ src/main/java/br/com/entities/House.java
@@ -0,0 +1,112 @@
+package br.com.entities;
+
+public class House {
+	
+	private String rua;
+	private Long numero;
+	private String bairro;
+	private String cidade;
+	private String UF;
+	
+	public House() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUF() {
+		return UF;
+	}
+
+	public void setUF(String uF) {
+		UF = uF;
+	}
+
+	@Override
+	public String toString() {
+		return "House [rua=" + rua + ", numero=" + numero + ", bairro="
+				+ bairro + ", cidade=" + cidade + ", UF=" + UF + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((UF == null) ? 0 : UF.hashCode());
+		result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		result = prime * result + ((rua == null) ? 0 : rua.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		House other = (House) obj;
+		if (UF == null) {
+			if (other.UF != null)
+				return false;
+		} else if (!UF.equals(other.UF))
+			return false;
+		if (bairro == null) {
+			if (other.bairro != null)
+				return false;
+		} else if (!bairro.equals(other.bairro))
+			return false;
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade))
+			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		if (rua == null) {
+			if (other.rua != null)
+				return false;
+		} else if (!rua.equals(other.rua))
+			return false;
+		return true;
+	}
+	
+	
+
+}
