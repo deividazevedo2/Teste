9410e30796a838586b7a0da3ecc4d6a566b55437
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-16 12:49:58 -0300

    novo atributo adicionado

--- src/main/java/br/com/entities/User.java
+++ src/main/java/br/com/entities/User.java
@@ -2,6 +2,7 @@ package br.com.entities;
 
 public class User {
 
+	private Integer id;
 	private String firstName;
 	private String lastName;
 	private Integer age;
@@ -50,11 +51,20 @@ public class User {
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	@Override
 	public String toString() {
-		return "User [firstName=" + firstName + ", lastName=" + lastName
-				+ ", age=" + age + ", cpf=" + cpf + ", rg=" + rg + "]";
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", age=" + age + ", cpf=" + cpf + ", rg=" + rg
+				+ "]";
 	}
 
 	@Override
@@ -65,6 +75,7 @@ public class User {
 		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
 		result = prime * result
 				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result
 				+ ((lastName == null) ? 0 : lastName.hashCode());
 		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
@@ -95,6 +106,11 @@ public class User {
 				return false;
 		} else if (!firstName.equals(other.firstName))
 			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
 		if (lastName == null) {
 			if (other.lastName != null)
 				return false;
@@ -107,5 +123,6 @@ public class User {
 			return false;
 		return true;
 	}
+
 
 }
