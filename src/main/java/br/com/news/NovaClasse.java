2c3aacc97d400be1405459ee19c13ed73f1e9c15
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-19 23:15:37 -0300

    New comit test

---
+++ src/main/java/br/com/news/NovaClasse.java
@@ -0,0 +1,19 @@
+package br.com.news;
+
+public class NovaClasse {
+
+	private String nova;
+
+	public NovaClasse(String nova) {
+		this.nova = nova;
+	}
+
+	public String getNova() {
+		return nova;
+	}
+
+	public void setNova(String nova) {
+		this.nova = nova;
+	}
+
+}
