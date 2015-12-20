5b6e4944113ab6397024f84ea99723c0ffacb683
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-19 23:12:38 -0300

    create Animal class

---
+++ src/main/java/br/com/entities/Animal.java
@@ -0,0 +1,65 @@
+package br.com.entities;
+
+public class Animal {
+	
+	private String gato;
+	private String cachorro;
+	private String cavalo;
+	private String urso;
+	private String bode;
+	private String cabra;
+	
+	public Animal() {
+	}
+
+	public String getGato() {
+		return gato;
+	}
+
+	public void setGato(String gato) {
+		this.gato = gato;
+	}
+
+	public String getCachorro() {
+		return cachorro;
+	}
+
+	public void setCachorro(String cachorro) {
+		this.cachorro = cachorro;
+	}
+
+	public String getCavalo() {
+		return cavalo;
+	}
+
+	public void setCavalo(String cavalo) {
+		this.cavalo = cavalo;
+	}
+
+	public String getUrso() {
+		return urso;
+	}
+
+	public void setUrso(String urso) {
+		this.urso = urso;
+	}
+
+	public String getBode() {
+		return bode;
+	}
+
+	public void setBode(String bode) {
+		this.bode = bode;
+	}
+
+	public String getCabra() {
+		return cabra;
+	}
+
+	public void setCabra(String cabra) {
+		this.cabra = cabra;
+	}
+	
+	
+
+}
