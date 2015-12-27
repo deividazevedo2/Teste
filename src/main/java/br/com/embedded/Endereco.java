4b7e3dd870db05df10f7633e1a9410594bf34ef4
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-27 18:29:01 -0300

    class embbeded

---
+++ src/main/java/br/com/embedded/Endereco.java
@@ -0,0 +1,12 @@
+package br.com.embedded;
+
+public class Endereco {
+	
+	private String rua;
+	private Integer numero;
+	private String bairro;
+	private String cidade;
+	private String CEP;
+	private String UF;
+
+}
