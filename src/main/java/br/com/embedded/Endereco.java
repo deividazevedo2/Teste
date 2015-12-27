6eaad12696c909b2d095c7fef181e26fd20fce92
Deivid Azevedo <deividazevedo1@gmail.com>
2015-12-27 18:29:27 -0300

    class embbeded

--- src/main/java/br/com/embedded/Endereco.java
+++ src/main/java/br/com/embedded/Endereco.java
@@ -8,5 +8,58 @@ public class Endereco {
 	private String cidade;
 	private String CEP;
 	private String UF;
+	
+	public Endereco() {
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getCEP() {
+		return CEP;
+	}
+
+	public void setCEP(String cEP) {
+		CEP = cEP;
+	}
+
+	public String getUF() {
+		return UF;
+	}
+
+	public void setUF(String uF) {
+		UF = uF;
+	}
+	
+	
 
 }
